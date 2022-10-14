4594ad99fef6d3ae191f9ca5755e81800271dbea
Darmatage <JasonWiserArt@gmail.com>
2022-10-14 00:18:41 -0400

    Added Pause Menu to all scenes

--- MA2_MidtermStory/Assets/Scripts/GameHandler.cs
+++ MA2_MidtermStory/Assets/Scripts/GameHandler.cs
@@ -7,80 +7,82 @@ using UnityEngine.Audio;
 
 public class GameHandler : MonoBehaviour {
 
-        public static int playerStat;
+	public static int playerStat;
+	public static bool GameisPaused = false;
+	public GameObject pauseMenuUI;
+	public AudioMixer mixer;
+	public static float volumeLevel = 1.0f;
+	private Slider sliderVolumeCtrl;
 
+	void Awake (){
+		SetLevel (volumeLevel);
+		GameObject sliderTemp = GameObject.FindWithTag("PauseMenuSlider");
+		if (sliderTemp != null){
+			sliderVolumeCtrl = sliderTemp.GetComponent<Slider>();
+			sliderVolumeCtrl.value = volumeLevel;
+		}
+	}
 
-        public static bool GameisPaused = false;
-        public GameObject pauseMenuUI;
-        public AudioMixer mixer;
-        public static float volumeLevel = 1.0f;
-        private Slider sliderVolumeCtrl;
+	void Start (){
+		pauseMenuUI.SetActive(false);
+		GameisPaused = false;
+	}
 
-        void Awake (){
-                SetLevel (volumeLevel);
-                GameObject sliderTemp = GameObject.FindWithTag("PauseMenuSlider");
-                if (sliderTemp != null){
-                        sliderVolumeCtrl = sliderTemp.GetComponent<Slider>();
-                        sliderVolumeCtrl.value = volumeLevel;
-                }
-        }
+	void Update (){
+		if (Input.GetKeyDown(KeyCode.Escape)){
+			if (GameisPaused){
+				Resume();
+			}
+			else{
+				Pause();
+			}
+		}
+	}
 
-        void Start (){
-                pauseMenuUI.SetActive(false);
-                GameisPaused = false;
-        }
+	void Pause(){
+		pauseMenuUI.SetActive(true);
+		Time.timeScale = 0f;
+		GameisPaused = true;
+	}
 
-        void Update (){
-                if (Input.GetKeyDown(KeyCode.Escape)){
-                        if (GameisPaused){
-                                Resume();
-                        }
-                        else{
-                                Pause();
-                        }
-                }
-        }
+	public void Resume(){
+		pauseMenuUI.SetActive(false);
+		Time.timeScale = 1f;
+		GameisPaused = false;
+	}
 
-        void Pause(){
-                pauseMenuUI.SetActive(true);
-                Time.timeScale = 0f;
-                GameisPaused = true;
-        }
+	public void SetLevel (float sliderValue){
+		mixer.SetFloat("MusicVolume", Mathf.Log10 (sliderValue) * 20);
+		volumeLevel = sliderValue;
+	}
 
-        public void Resume(){
-                pauseMenuUI.SetActive(false);
-                Time.timeScale = 1f;
-                GameisPaused = false;
-        }
+	public void UpdatePlayerStat(int amount){
+		playerStat += amount;
+		Debug.Log("Current Player Stat = " + playerStat);
+	}
 
-        public void SetLevel (float sliderValue){
-                mixer.SetFloat("MusicVolume", Mathf.Log10 (sliderValue) * 20);
-                volumeLevel = sliderValue;
-        }
+	public int CheckPlayerStat(){
+		return playerStat;
+	}
 
-        public void UpdatePlayerStat(int amount){
-                playerStat += amount;
-                Debug.Log("Current Player Stat = " + playerStat);
-        }
+	public void StartGame(){
+		SceneManager.LoadScene("Scene1");
+	}
 
-        public int CheckPlayerStat(){
-                return playerStat;
-        }
+	public void OpenCredits(){
+		SceneManager.LoadScene("Credits");
+	}
 
-        public void StartGame(){
-                SceneManager.LoadScene("Scene1");
-        }
+	public void RestartGame(){
+		Time.timeScale = 1f;
+		SceneManager.LoadScene("MainMenu");
+	}
 
-        public void RestartGame(){
-                Time.timeScale = 1f;
-                SceneManager.LoadScene("MainMenu");
-        }
-
-        public void QuitGame(){
-                #if UNITY_EDITOR
-                UnityEditor.EditorApplication.isPlaying = false;
-                #else
-                Application.Quit();
-                #endif
-        }
+	public void QuitGame(){
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#else
+		Application.Quit();
+		#endif
+	}
 }
