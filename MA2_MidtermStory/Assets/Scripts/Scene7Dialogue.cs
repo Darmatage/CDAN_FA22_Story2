01595789e604e71423f2cc51c9093a8540cb4907
livthealien <fairolivia4@gmail.com>
2022-10-27 01:19:36 -0400

    Fixed Scene7 script and inspector

--- MA2_MidtermStory/Assets/Scripts/Scene7Dialogue.cs
+++ MA2_MidtermStory/Assets/Scripts/Scene7Dialogue.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEngine.Audio;
+
 public class Scene7Dialogue : MonoBehaviour {
         public int primeInt = 1;         // This integer drives game progress!
         public Text Char1name;
@@ -13,22 +14,19 @@ public class Scene7Dialogue : MonoBehaviour {
        //public Text Char3name;
        //public Text Char3speech;
         public GameObject DialogueDisplay;
-        public GameObject ArtChar1;
+        public GameObject ArtChar1a;
 		    public GameObject ArtChar1b;
         public GameObject ArtChar1c;
         public GameObject ArtChar1d;
         public GameObject ArtChar1e;
 		//public GameObject ArtChar1c;
-       public GameObject ArtChar2;
 	   //public GameObject ArtChar2b;
 	   //public GameObject ArtChar2c;
         public GameObject ArtBG1;
-        public GameObject ArtBG2;
         public GameObject Choice1a;
         public GameObject Choice1b;
-		    public GameObject Choice1c;
-        public GameObject Choice1d;
         public GameObject NextScene1Button;
+        public GameObject NextScene2Button;
         //public GameObject NextScene2Button;
         public GameObject nextButton;
         public GameHandler gameHandler;
@@ -40,14 +38,16 @@ public class Scene7Dialogue : MonoBehaviour {
 
 void Start(){         // initial visibility settings
         DialogueDisplay.SetActive(false);
-        ArtChar1.SetActive(false);
+        ArtChar1a.SetActive(false);
 		    ArtChar1b.SetActive(false);
-		    ArtChar2.SetActive(false);
+        ArtChar1c.SetActive(false);
+        ArtChar1d.SetActive(false);
+        ArtChar1e.SetActive(false);
         ArtBG1.SetActive(true);
         Choice1a.SetActive(false);
         Choice1b.SetActive(false);
-		    Choice1c.SetActive(false);
         NextScene1Button.SetActive(false);
+        NextScene2Button.SetActive(false);
         //NextScene2Button.SetActive(false);
         nextButton.SetActive(true);
    }
@@ -72,7 +72,7 @@ public void talking(){         // main story function. Players hit next to progr
                 // AudioSource.Play();
     }
         else if (primeInt == 2){
-          ArtChar1.SetActive(true);
+          ArtChar1a.SetActive(true);
                 DialogueDisplay.SetActive(true);
                 Char1name.text = "";
                 Char1speech.text = "<Amazing smell leads you to Chris's kitchen when Christ notices you>";
@@ -89,15 +89,11 @@ public void talking(){         // main story function. Players hit next to progr
                 if (GameHandler.hasProof == true){
                   Choice1a.SetActive(true); // bark
                   Choice1b.SetActive(true);
-                  Choice1c.SetActive(false);
-                  Choice1d.SetActive(false);
                   Debug.Log("hasProof is true, showing bark button");
                 }
                 else if ((GameHandler.hasComm == true)||(GameHandler.hasAi == true)){
                   Choice1a.SetActive(false); // bark
                   Choice1b.SetActive(false);
-                  Choice1c.SetActive(true);
-                  Choice1d.SetActive(true);
                   Debug.Log("hasComm or hasAi is true, showing talk button");
                 }
 
@@ -135,8 +131,6 @@ public void talking(){         // main story function. Players hit next to progr
                 allowSpace = false;
                 Choice1a.SetActive(true); // function Choice1aFunct()
                 Choice1b.SetActive(true); // function Choice1bFunct()
-			        	Choice1c.SetActive(true); // function Choice1bFunct()
-                Choice1d.SetActive(true);// function Choice1dFunct()
         }
 
 
@@ -172,9 +166,7 @@ public void talking(){         // main story function. Players hit next to progr
           Char1speech.text = "";
           Char2name.text = "";
           Char2speech.text = "";
-          StartCoroutine(FadeOut(ArtBG2));
-
-        }
+       }
 
 
       //Encounter after choice2 to reject:
@@ -294,7 +286,6 @@ public void talking(){         // main story function. Players hit next to progr
                allowSpace = false;
                Choice1a.SetActive(true); // function Choice1aFunct()
                Choice1b.SetActive(true); // function Choice1bFunct()
-
        }
 
        //Choice b (reject)for without communication device:
@@ -334,7 +325,6 @@ public void talking(){         // main story function. Players hit next to progr
                     primeInt = 499;
                     Choice1a.SetActive(false);
                     Choice1b.SetActive(false);
-    				        Choice1c.SetActive(false);
                     nextButton.SetActive(true);
                     allowSpace = true;
     				//GameObject.FindWithTag("GameHandler").GetComponent<GameHandler>().hasComm = true;
@@ -347,7 +337,6 @@ public void talking(){         // main story function. Players hit next to progr
                     primeInt = 699;
                     Choice1a.SetActive(false);
                     Choice1b.SetActive(false);
-    				        Choice1c.SetActive(false);
                     nextButton.SetActive(true);
                     allowSpace = true;
                     //GameObject.FindWithTag("GameHandler").GetComponent<GameHandler>().hasComm = true;
@@ -365,7 +354,6 @@ public void talking(){         // main story function. Players hit next to progr
                 primeInt = 499;
                 Choice1a.SetActive(false);
                 Choice1b.SetActive(false);
-				        Choice1c.SetActive(false);
                 nextButton.SetActive(true);
                 allowSpace = true;
 				//GameObject.FindWithTag("GameHandler").GetComponent<GameHandler>().hasComm = true;
@@ -378,7 +366,6 @@ public void talking(){         // main story function. Players hit next to progr
                 primeInt = 599;
                 Choice1a.SetActive(false);
                 Choice1b.SetActive(false);
-				        Choice1c.SetActive(false);
                 nextButton.SetActive(true);
                 allowSpace = true;
                 //GameObject.FindWithTag("GameHandler").GetComponent<GameHandler>().hasComm = true;
